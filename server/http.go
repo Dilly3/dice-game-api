d2dbde754cfb0fdd358a6002aa139f49b44e9a79
michael anikammadu <dil.anikamadu@gmail.com>
2023-07-10 02:17:07 +0100

    api handler

--- server/http.go
+++ server/http.go
@@ -1,354 +1,119 @@
 package server
 
 import (
-	"context"
 	"fmt"
+	"log"
 
-	"time"
-
-	db "github.com/dilly3/dice-game-api/db/sqlc"
-	"github.com/dilly3/dice-game-api/game"
-	"github.com/dilly3/dice-game-api/service"
-	"github.com/dilly3/dice-game-api/util"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"go.uber.org/zap"
 )
 
-func GetUsers() func(*fiber.Ctx) error {
-	return func(c *fiber.Ctx) error {
+type Routes []Route
+type handler []func(c *fiber.Ctx) error
 
-		users, err := service.DefaultGameService.GetAllUsers(context.Background(), db.ListUsersParams{Limit: 10, Offset: 0})
+var ServerInst *Server
 
-		if err != nil {
-			return err
-		}
+type Server struct {
+	Router *fiber.App
+	Port   string
+	Logger *zap.Logger
+}
 
-		return util.Response(c, "users", fiber.StatusOK, users, 0, nil)
+func NewServer(port string, router *fiber.App) *Server {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		fmt.Printf("error setting up Logger =>%v\n", err)
+		log.Fatal(err)
+	}
+	return &Server{
+		Router: router,
+		Port:   port,
+		Logger: logger,
 	}
 }
 
-func Register() func(*fiber.Ctx) error {
-	return func(c *fiber.Ctx) error {
-		user := &db.RegisterUserDto{}
-		if err := c.BodyParser(user); err == fiber.ErrUnprocessableEntity {
-			return util.ErrorResponse(c, "invalid json", fiber.StatusBadRequest)
-
-		}
-
-		dbuser, _ := service.DefaultGameService.GetUserByUsername(context.Background(), user.Username)
-		if dbuser.ID != 0 {
-			return util.ErrorResponse(c, "username already exists", fiber.StatusBadRequest)
-
-		}
-
-		if user.Password != user.ConfirmPassword {
-			return util.ErrorResponse(c, "passwords do not match", fiber.StatusBadRequest)
-
-		}
-
-		dbuser, err := service.DefaultGameService.CreateUser(db.CreateUserParams{
-			Firstname: user.Firstname,
-			Lastname:  user.Lastname,
-			Username:  user.Username,
-			Password:  user.Password,
-		})
-
-		if err != nil {
-			return util.ErrorResponse(c, "internal Server error :"+err.Error(), fiber.StatusBadRequest)
-
-		}
-
-		return util.Response(c, "user created successfully", fiber.StatusCreated, user, 0, nil)
-	}
+func (s *Server) ListenAndServe() error {
+	return s.Router.Listen(fmt.Sprintf(":%s", s.Port))
 }
 
-func Login() func(*fiber.Ctx) error {
-	return func(c *fiber.Ctx) error {
-		loginbody := &db.LoginDto{}
-		if err := c.BodyParser(loginbody); err == fiber.ErrUnprocessableEntity {
-			return util.ErrorResponse(c, "bad login credentials", fiber.StatusBadRequest)
+func (s *Server) StartServer() {
+	//  start user service with default repo instance
 
-		}
-		dbuser := db.User{}
-		// Get first matcsed record
-		dbuser, err := service.DefaultGameService.GetUserByUsername(context.Background(), loginbody.Username)
-		if err != nil {
-			return util.ErrorResponse(c, "email or password incorrect", fiber.StatusBadRequest)
+	s.Router.Use(logger.New(logger.Config{
+		Format:     " ${pid} Time:${time} Status: ${status} - ${method} ${path}\n",
+		TimeFormat: "02-Jan-2006 15:04:05",
+		TimeZone:   "GMT+1",
+	}))
 
-		}
+	v1 := s.Router.Group("api/v1")
 
-		if err := dbuser.CompareHashAndPassword(loginbody.Password); err != nil || dbuser.ID == 0 {
-			return util.ErrorResponse(c, "email or password incorrect", fiber.StatusBadRequest)
-
-		}
-
-		c.Cookie(&fiber.Cookie{Name: "user", Value: dbuser.Username, HTTPOnly: true, Expires: time.Now().Add(time.Hour * 24)})
-
-		return util.Response(c, "login successful", fiber.StatusOK, nil, 0, nil)
-
+	for _, route := range routes {
+		v1.Add(route.Method, route.Path, route.Handler...)
 	}
+
 }
 
-func GetWalletBalance() func(*fiber.Ctx) error {
-	return func(c *fiber.Ctx) error {
-		username := c.Cookies("user")
-		if username == "" {
-			return util.ErrorResponse(c, "user not logged in", fiber.StatusForbidden)
-
-		}
-
-		bal, _, err := service.DefaultGameService.GetWalletBalance(username)
-
-		if err != nil && err.Error() == "cant get wallet : sql: no rows in result set" {
-			c.SendStatus(fiber.StatusBadRequest)
-			return util.ErrorResponse(c, "user not available", fiber.StatusBadRequest)
-
-		}
-
-		if err != nil {
-			c.SendStatus(fiber.StatusInternalServerError)
-			return util.ErrorResponse(c, "internal Server error :"+err.Error(), fiber.StatusInternalServerError)
-
-		}
-
-		strbal := fmt.Sprintf("%d", bal)
-		return util.WalletBallanceResponse(c, strbal)
-
-	}
+type Route struct {
+	Method  string
+	Path    string
+	Handler handler
 }
 
-func CreditWallet() func(*fiber.Ctx) error {
-	return func(c *fiber.Ctx) error {
-		username := c.Cookies("user")
-		if username == "" {
-
-			return util.ErrorResponse(c, "user not logged in", fiber.StatusForbidden)
-
-		}
-
-		err := service.DefaultGameService.CreditWallet(username, 155)
-
-		if err != nil && err.Error() == "sql: no rows in result set" {
-
-			return util.ErrorResponse(c, "user not available", fiber.StatusBadRequest)
-
-		}
-
-		if err != nil {
-			c.SendStatus(fiber.StatusInternalServerError)
-			return util.ErrorResponse(c, "internal *Server error :"+err.Error(), fiber.StatusInternalServerError)
-
-		}
-
-		return util.Response(c, "wallet credited", fiber.StatusOK, nil, 0, nil)
-	}
-}
-
-func DebitWallet() func(*fiber.Ctx) error {
-	return func(c *fiber.Ctx) error {
-
-		username := c.Cookies("user")
-		if username == "" {
-			return util.ErrorResponse(c, "user not logged in", fiber.StatusForbidden)
-
-		}
-
-		body := &db.CreateWalletDto{}
-		if err := c.BodyParser(body); err == fiber.ErrUnprocessableEntity {
-			c.SendStatus(fiber.StatusBadRequest)
-			return util.ErrorResponse(c, "bad request", fiber.StatusBadRequest)
-
-		}
-
-		err := service.DefaultGameService.DebitWallet(username, body.Amount)
-
-		if err != nil && err.Error() == "sql: no rows in result set" {
-			c.SendStatus(fiber.StatusBadRequest)
-			return c.JSON(fiber.Map{"message": "user not available"})
-
-		}
-
-		if err != nil {
-			c.SendStatus(fiber.StatusInternalServerError)
-			return c.JSON(fiber.Map{"message": "internal *Server error :" + err.Error()})
-
-		}
-
-		return c.JSON("successful")
-	}
-}
-
-func GetSessionState() func(*fiber.Ctx) error {
-	return func(c *fiber.Ctx) error {
-
-		username := c.Cookies("user")
-		if username == "" {
-			c.SendStatus(fiber.StatusForbidden)
-			return c.JSON(fiber.Map{"message": "user not logged in"})
-		}
-
-		return c.JSON(fiber.Map{"isSessionActive": game.GameConfig.IsGameInSession})
-	}
-}
-
-func StartGame() func(*fiber.Ctx) error {
-	return func(c *fiber.Ctx) error {
-		user := c.Cookies("user")
-		if user == "" {
-			c.SendStatus(fiber.StatusForbidden)
-			return c.JSON(fiber.Map{"message": "user not logged in"})
-		}
-		if game.GameConfig.IsGameInSession {
-			return c.JSON(fiber.Map{"message": "game already in session"})
-		}
-
-		err := service.DefaultGameService.DebitWallet(user, 20)
-
-		if err != nil && err.Error() == "sql: no rows in result set" {
-			return c.JSON(fiber.Map{"message": "user not available"})
-
-		}
-
-		if err != nil {
-			return util.ErrorResponse(c, "internal Server error :"+err.Error(), fiber.StatusInternalServerError)
-
-		}
-
-		game.StartGame()
-		game.GameConfig.IsGameInSession = true
-		jackpotNum := game.GameConfig.LuckyNumber
-		return util.Response(c, "game started, debit 20 sats, roll dice. good luck!", fiber.StatusOK, nil, jackpotNum, nil)
-
-	}
-}
-
-func RollDice() func(*fiber.Ctx) error {
-	return func(c *fiber.Ctx) error {
-
-		user := c.Cookies("user")
-		if user == "" {
-			return util.ErrorResponse(c, "user not logged in", fiber.StatusForbidden)
-		}
-		if !game.GameConfig.IsGameInSession {
-
-			return util.ErrorResponse(c, "game not in session, start game first", fiber.StatusBadRequest)
-
-		}
-
-		if game.GameConfig.RollNumber1 == 0 {
-			err := service.DefaultGameService.DebitWallet(user, 5)
-
-			if err != nil && err.Error() == "sql: no rows in result set" {
-				return util.ErrorResponse(c, "user not available", fiber.StatusBadRequest)
-
-			}
-			if err != nil {
-
-				return util.ErrorResponse(c, "internal *Server error :"+err.Error(), fiber.StatusInternalServerError)
-
-			}
-			// roll dice 1
-			game.RollDice1()
-			num1 := game.GameConfig.RollNumber1
-			if num1 > game.GameConfig.LuckyNumber {
-				game.ResetRoll()
-				return util.RollResponse(c, "you Lost, first roll is greater than jackpot number", num1, 0)
-
-			}
-
-			if num1 == game.GameConfig.LuckyNumber {
-				game.ResetRoll()
-				return util.RollResponse(c, "you Lost, first roll is equal to jackpot number", num1, 0)
-			}
-
-			if game.GameConfig.LuckyNumber-num1 > 6 {
-				game.ResetRoll()
-				return util.RollResponse(c, "you Lost, u need more than 6 to sit jackpot number", num1, 0)
-
-			}
-
-			return util.RollResponse(c, fmt.Sprintf("you need %d to win", game.GameConfig.LuckyNumber-num1), num1, 0)
-
-		}
-
-		// roll dice 2
-
-		game.RollDice2()
-
-		temp2 := game.GameConfig.RollNumber2
-		temp1 := game.GameConfig.RollNumber1
-
-		if game.GameConfig.RollNumber2 != 0 && game.GameConfig.RollNumber1+game.GameConfig.RollNumber2 == game.GameConfig.LuckyNumber {
-			err := service.DefaultGameService.CreditWalletForWin(user, 10)
-			if err != nil {
-				return util.ErrorResponse(c, "internal server error "+err.Error(), fiber.StatusInternalServerError)
-
-			}
-			game.ResetRoll()
-			return util.RollResponse(c, "WIN WIN WIN !!!!!!, You won 10 sats", temp1, temp2)
-		}
-
-		game.ResetRoll()
-		return util.RollResponse(c, "you lost", temp1, temp2)
-
-	}
-}
-
-func StopGame() func(*fiber.Ctx) error {
-	return func(c *fiber.Ctx) error {
-		user := c.Cookies("user")
-		if user == "" {
-			c.SendStatus(fiber.StatusForbidden)
-			return c.JSON(fiber.Map{"message": "user not logged in"})
-		}
-		if !game.GameConfig.IsGameInSession {
-			c.Status(fiber.StatusUnauthorized)
-			return util.Response(c, "game not in session, start game first", fiber.StatusBadRequest, nil, 0, nil)
-
-		}
-
-		game.StopGame()
-		return util.Response(c, "game stopped", fiber.StatusOK, nil, 0, nil)
-
-	}
-}
-
-func Logout() func(*fiber.Ctx) error {
-	return func(c *fiber.Ctx) error {
-		user := c.Cookies("user")
-		if user == "" {
-			c.SendStatus(fiber.StatusForbidden)
-			return util.ErrorResponse(c, "user not logged in", fiber.StatusForbidden)
-
-		}
-		c.Cookie(&fiber.Cookie{
-			Value:   "",
-			Name:    "user",
-			Expires: time.Now().Add(-time.Hour),
-		})
-		game.StopGame()
-		return util.Response(c, "logged out", fiber.StatusOK, nil, 0, nil)
-
-	}
-}
-
-func GetTransactions() func(*fiber.Ctx) error {
-	return func(c *fiber.Ctx) error {
-		user := c.Cookies("user")
-		if user == "" {
-			return util.ErrorResponse(c, "user not logged in", fiber.StatusForbidden)
-
-		}
-		transactions, err := service.DefaultGameService.GetTransactionHistory(user)
-		if err != nil && err.Error() == "sql: no rows in result set" {
-
-			return util.ErrorResponse(c, "user not available", fiber.StatusBadRequest)
-
-		}
-
-		if err != nil {
-			return util.ErrorResponse(c, "internal *Server error :"+err.Error(), fiber.StatusInternalServerError)
-
-		}
-		return util.Response(c, "transactions", fiber.StatusOK, transactions, 0, nil)
-	}
+var routes = Routes{
+	Route{
+		Method:  "POST",
+		Path:    "/register",
+		Handler: handler{Register()},
+	},
+	Route{
+		Method:  "POST",
+		Path:    "/login",
+		Handler: handler{Login()},
+	},
+	Route{
+		Method:  "GET",
+		Path:    "/all",
+		Handler: handler{GetUsers()},
+	},
+	Route{
+		Method:  "GET",
+		Path:    "/balance",
+		Handler: handler{GetWalletBalance()},
+	},
+	Route{
+		Method:  "GET",
+		Path:    "/credit",
+		Handler: handler{CreditWallet()},
+	},
+	Route{
+		Method:  "GET",
+		Path:    "/roll",
+		Handler: handler{RollDice()},
+	},
+	Route{
+		Method:  "GET",
+		Path:    "/start",
+		Handler: handler{StartGame()},
+	},
+	Route{
+		Method:  "GET",
+		Path:    "/session",
+		Handler: handler{GetSessionState()},
+	},
+	Route{
+		Method:  "GET",
+		Path:    "/end",
+		Handler: handler{StopGame()},
+	},
+	Route{
+		Method:  "GET",
+		Path:    "/logout",
+		Handler: handler{Logout()},
+	},
+	Route{
+		Method:  "GET",
+		Path:    "/transactions",
+		Handler: handler{GetTransactions()},
+	},
 }
